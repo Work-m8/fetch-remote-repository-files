e4c93282752bfaebd2c843cf52954dcf27969886
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2024-02-17 11:06:44 +0100

    Added download script

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,38 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { GitHubDownloader } from './downloader'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const GH_TOKEN: string = core.getInput('GH_TOKEN')
+    const REPOSITORY: string = core.getInput('repository')
+    const ORGANIZATION: string = core.getInput('organization')
+    const SHA: string = core.getInput('sha')
+    const INPUT_PATH: string = core.getInput('input_path')
+    const OUTPUT_PATH: string = core.getInput('output_path')
+    const FILES: string[] = core.getInput('files').split(',')
+    const EXTENSIONS: string[] = core.getInput('extensions').split(',')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const downloader: GitHubDownloader = new GitHubDownloader({
+      repository: {
+        repository: REPOSITORY,
+        organization: ORGANIZATION,
+        sha: SHA
+      },
+      GH_TOKEN: GH_TOKEN,
+      input_path: INPUT_PATH,
+      output_path: OUTPUT_PATH,
+      filter: {
+        files: FILES,
+        extensions: EXTENSIONS
+      }
+    })
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`Starting to download files from ${ORGANIZATION}/${REPOSITORY}`)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    await downloader.downloadFolder()
+
+    core.debug('Done downloading files')
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
